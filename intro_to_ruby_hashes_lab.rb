95f02010a374305d897b08a3b118a5549d885c6d
Melinda Diaz <melindadiaz@me.com>
2020-07-19 22:03:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ def monopoly_with_second_tier
     base_hash[:railroads][:pieces] = 4
     base_hash[:railroads][:rent_in_dollars] = {}
     base_hash[:railroads][:names] = {}
-    
+    base_hash
 end
 
 def monopoly_with_third_tier
