578473ee6e8541cabb01d6b611ea78305b628b42
Melinda Diaz <melindadiaz@me.com>
2020-07-19 22:01:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,6 +11,8 @@ def monopoly_with_second_tier
   # complexity.
     
     base_hash[:railroads][:pieces] = 4
+    base_hash[:railroads][:rent_in_dollars] = {}
+    base_hash[:railroads][:names] = {}
     
 end
 
