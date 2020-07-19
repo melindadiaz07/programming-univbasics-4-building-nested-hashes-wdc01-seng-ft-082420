a4b76d52946ed0df4c649fe6310a286c80db499d
Melinda Diaz <melindadiaz@me.com>
2020-07-19 22:07:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,7 @@ def monopoly_with_third_tier
   }
     
     base_hash[:railroads][:pieces] = 4
-    base_hash[:railroads][:rent_in_dollars] = {one_piece_owned: 25, two_pieces_owned: 50}
+    base_hash[:railroads][:rent_in_dollars] = {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100}
     base_hash[:railroads][:names] = {}
     
     base_hash
